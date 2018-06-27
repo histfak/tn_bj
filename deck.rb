29c5c9a630c7017754092fa18ddfb1a2c4bd1ce2
histfak@gmail.com <histfak@gmail.com>
2018-06-27 12:11:42 +0300

    card and deck refactoring

--- deck.rb
+++ deck.rb
@@ -3,17 +3,14 @@ require_relative 'card'
 class Deck
   attr_accessor :deck
 
-  RANKS = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
-  SUITS = %w[♠ ♣ ♦ ♥].freeze
-
   def initialize
     @deck = Deck.new_deck
   end
 
   def self.new_deck
     @deck = []
-    SUITS.each do |suit|
-      RANKS.each do |rank|
+    Card::SUITS.each do |suit|
+      Card::RANKS.each do |rank|
         @deck << Card.new(rank, suit)
       end
     end
